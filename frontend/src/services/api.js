d4f7f06e68d9d477ff6bc1afb2bc1d4d45729a66
krishnaa-1 <krishnamalviya272@gmail.com>
2025-03-13 16:19:58 +0530

    donee

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `${import.meta.env.VITE_REACT_APP_API_URL}api`
+const API_URL = `${import.meta.env.VITE_REACT_APP_API_URL}/api`
 
 
 // Fetch all recipes
